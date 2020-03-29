b81b0b45e62b84a34ce0d4147ef5d25dec803015
jashaikh-dev <junaidazharshaikh@gmail.com>
2020-03-29 16:38:44 -0400

    feat: initial api setup

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/api/v1')
+def greeting():
+  return 'Welcome JW Machine Learning API'
+
+
+@app.route('/api/v1/getsentiment', methods=['POST'])
+def getSentiment():
+  sentence = request.json['sentence']
+  # you need to perform sentiment analysis here.
+  return sentence 
